6f8f14b731b09860cdbd3fbb1be0d0898a35a5c1
Ayan-004 <ayyanjsk@gmail.com>
2024-07-08 17:04:35 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => number
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = "Ayan"
+// console.log(typeof isLoggedIn);
+
+// let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof boolIsLoggedIn);
+// console.log(boolIsLoggedIn);
+
+// let someNumber = 10;
+// let convert = String(someNumber)
+// console.log(convert);
+// console.log(typeof convert);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ayan" => true
+
+// ******************************Operation**********************************
+
+let x = 3;
+let y = x++;
+//console.log(`x:${x}, y:${y}`);
+
+let a = 2;
+let b = ++a;
+//console.log(`a:${a}, b:${b}`);
