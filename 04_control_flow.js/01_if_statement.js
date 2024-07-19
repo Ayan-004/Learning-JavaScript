1a79454700fd294fd9ab57597c3e87c328422448
Ayan-004 <ayyanjsk@gmail.com>
2024-07-19 11:02:48 +0000

    if statement done

---
+++ 04_control_flow.js/01_if_statement.js
@@ -0,0 +1,44 @@
+// const userLoggedIn = true
+// const temperature = 60
+
+// if (temperature < 50) {
+    // console.log("less than 50");
+// } else{
+    // console.log("greater than 50");
+// }
+// console.log("Executed");
+// >, <, >=, <=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score >= 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// const balance = 300
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if(balance < 750) {
+//     console.log("less than 750");
+// } else if(balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+
+// if(userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy courses");
+// }
+
+const LoggedInFromGoogle = true
+const LoggedInFromEmail = true
+
+if (LoggedInFromGoogle || LoggedInFromEmail) {
+    console.log("User Logged In");
+}
