dcfecac6fcb57b03f79b86836d667f48fe2d3a92
Ayan-004 <ayyanjsk@gmail.com>
2024-07-20 18:28:12 +0000

    Switch and truthy Done

---
+++ 04_control_flow.js/03_truthy.js
@@ -0,0 +1,18 @@
+const userEmail = []
+
+if(userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyobj = {}
+
+if (Object.keys(emptyobj).length == 0) {
+    console.log("Object is empty");
+
+}
