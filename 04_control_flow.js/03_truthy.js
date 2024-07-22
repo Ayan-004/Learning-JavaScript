473275f773e521c3f2343f33e20c3b9b7e84c551
Ayan-004 <ayyanjsk@gmail.com>
2024-07-22 09:27:32 +0000

    Control Flow Complete

--- 04_control_flow.js/03_truthy.js
+++ 04_control_flow.js/03_truthy.js
@@ -1,9 +1,9 @@
 const userEmail = []
 
 if(userEmail) {
-    console.log("Got user email");
+    // console.log("Got user email");
 } else {
-    console.log("Don't have user email");
+    // console.log("Don't have user email");
 }
 
 // if (userEmail.length === 0) {
@@ -13,6 +13,20 @@ if(userEmail) {
 const emptyobj = {}
 
 if (Object.keys(emptyobj).length == 0) {
-    console.log("Object is empty");
+    // console.log("Object is empty");
 
 }
+
+//Nullish Coalescing Operator(??): null, undefined
+// let val1 = 5 ?? 10
+// let val1 = null ?? 10
+// let val1 = undefined ?? 10
+let val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+// Ternary Operator
+
+let iceTeaPrice = 100
+
+iceTeaPrice <=80 ? console.log("less than 80") : console.log("more than 80");
